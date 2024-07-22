de8de116573e06d38e0d764f459639c5caabf9da
Kaka <132884456+Kaka030@users.noreply.github.com>
2024-07-22 17:38:54 +0800

    Add files via upload

---
+++ AutoSave/AutoSave/AutoSaveConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Rocket.API;
+using System.Runtime.Serialization.Formatters;
+
+namespace kaka.AutoSave
+{
+    public class AutoSaveConfiguration : IRocketPluginConfiguration
+    {
+        public bool Enabled;
+        public int SaveInterval;
+        public string SaveMsg;
+        public string MessageColor;
+        public void LoadDefaults()
+        {
+            Enabled = true;
+            SaveInterval = 60;
+            SaveMsg = "服务器保存中...";
+            MessageColor = "blue";
+        }
+    }
+}
